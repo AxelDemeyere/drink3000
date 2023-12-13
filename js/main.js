66e16a807e081f4683d491c4af51971bea8e27a0
AxelDemeyere <axel.demeyere59@gmail.Com>
2023-12-13 12:26:02 +0100

    ADD section html

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.querySelector("navbar").style.top = "0";
+    } else {
+      document.querySelector("navbar").style.top = "-50px";
+    }
+  }
