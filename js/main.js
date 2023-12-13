7e50a7bd3f3c81ffb65b9dd215f0d7a4093650ca
AxelDemeyere <axel.demeyere59@gmail.Com>
2023-12-13 17:32:54 +0100

    PUSH fin de journée 13/12/2023

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.querySelector("navbar").style.top = "0";
-    } else {
-      document.querySelector("navbar").style.top = "-50px";
-    }
-  }
 
-  document.querySelector('.down-arrow').addEventListener('click', function (event) {
+/* Pour navbar en scrollant */
+function scrollFunction() {
+    navbar = document.querySelector(".header-container");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.style.top = "0";
+    } else {
+        navbar.style.top = "-50px";
+    }
+}
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+/* Animation de scroll down pour flèche sur vidéo*/
+document.getElementById('scroll-anchor').addEventListener('click', function (event) {
     event.preventDefault();
-    const targetElement = document.querySelector('.scroll-anchor');
-    window.scrollTo({
-      bottom: targetElement.offsetTop,
-      behavior: 'smooth'
-    });
-  });
+    document.getElementById('recettes').scrollIntoView({ behavior: 'smooth' });
+});
+
+
+/*Fonction sélection de boisson*/
+
+function selectDrink(type) {
+    const cards = document.querySelectorAll('.cards');
+
+    cards.forEach(card => {
+        const cardtype = card.querySelector('header p').textContent;
+
+        if (cardtype == type) {
+            card.classList.add('selected');
+        } else {
+            card.classList.remove('selected');
+            card.classList.add('unavailable');
+            card.querySelector('infos p').textContent = "Non disponible, modifiez votre sélection pour sélectionner cette recette";
+        }
+    })
+}
+
