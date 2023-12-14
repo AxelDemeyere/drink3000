ba53a98fb19ff71cee87515aeff279c37f3f9686
Remar59 <remlevy@hotmail.fr>
2023-12-14 21:44:58 +0100

    Add more JS, tweaked Sass a bit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-const drinks = {
-    "1": "Original",
-    "2": "Mojito",
-    "3": "Tropical"
-};
-
-
 /* Pour navbar en scrollant */
 function scrollFunction() {
-    navbar = document.querySelector(".header");
-    header = document.querySelector(".header-logo");
-    if (document.documentElement.scrollTop < 200) {
-        navbar.style.display = "none";
-        
+    navbar = document.querySelector(".header-container");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.style.top = "0";
     } else {
-        navbar.style.display = "flex";
-        header.style.display = "none";
+        navbar.style.top = "-50px";
     }
 }
 
-window.onscroll = function() {
+window.onscroll = function () {
     scrollFunction();
 };
 
@@ -29,18 +19,25 @@ document.getElementById('scroll-anchor').addEventListener('click', function (eve
 });
 
 
- /*Fonction sélection de boisson, doit fonctionner aussi sur les catégories*/
+/*Fonction sélection de boisson et affichage dans div choice*/
 
- function displayChoice() {
-    let btn2 = document.querySelector("#btnm");
+function displayChoice() {
+    let btn2 = document.querySelectorAll(".btn2");
+    let titleChoice = document.querySelector(".titleChoice");
     let choice = document.querySelector(".choice");
 
-    btn2.addEventListener("click", () => {
-        choice.innerHTML = `${drinks[2]}`;
+    btn2.forEach(button => {
+        button.addEventListener("click", () => {
+            let selection1 = button.closest(".card").querySelector(".name").textContent;
+            let selection2 = button.closest(".card").querySelector(".infos").textContent;
+            titleChoice.innerHTML = selection1;
+            choice.innerHTML = selection2;
+        });
     });
 }
 
 displayChoice();
 
-
-
+function displayCategory() {
+    let
+}
